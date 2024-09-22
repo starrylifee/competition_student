fcb7e084ed0918c9ba08ca57a9d4bfafda41e15e
starrylife <forinnocen@naver.com>
2024-09-22 16:22:17 +0900

    color2

--- pages/4 chatbot.py
+++ pages/4 chatbot.py
@@ -16,33 +16,43 @@ st.set_page_config(
 # Streamlit의 배경색 변경
 background_color = "#F0FFF0"
 
+# 배경색 변경을 위한 CSS
+page_bg_css = f"""
+<style>
+    .stApp {{
+        background-color: {background_color};
+    }}
+</style>
+"""
+
 # Streamlit의 기본 메뉴와 푸터 숨기기
 hide_menu_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-</style>
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    var mainMenu = document.getElementById('MainMenu');
-    if (mainMenu) {
-        mainMenu.style.display = 'none';
-    }
-    var footer = document.getElementsByTagName('footer')[0];
-    if (footer) {
-        footer.style.display = 'none';
-    }
-    var header = document.getElementsByTagName('header')[0];
-    if (header) {
-        header.style.display = 'none';
-    }
-});
-</script>
+    <style>
+    #MainMenu {visibility: hidden; }
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var mainMenu = document.getElementById('MainMenu');
+        if (mainMenu) {
+            mainMenu.style.display = 'none';
+        }
+        var footer = document.getElementsByTagName('footer')[0];
+        if (footer) {
+            footer.style.display = 'none';
+        }
+        var header = document.getElementsByTagName('header')[0];
+        if (header) {
+            header.style.display = 'none';
+        }
+    });
+    </script>
 """
 
 # Streamlit에서 HTML 및 CSS 적용
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.markdown(page_bg_css, unsafe_allow_html=True)
 
 # OpenAI API 클라이언트 초기화
 api_keys = st.secrets["api"]["keys"]
